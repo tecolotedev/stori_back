c89283d702a4f5bfceb1ee4ef59b01ed60124a77
Jose Manuel Escobedo Lopez <teco@Joses-MacBook-Air.local>
2023-12-27 16:15:01 -0600

    change origins

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "*",
+		AllowOrigins:     "http://localhost:3000, https://stori-front.tecolotedev.com",
 		AllowHeaders:     "Origin, Content-Type, Accept",
 		AllowCredentials: true,
 	}))
