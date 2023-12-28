f968400c8271a4e1c0dc72088d2d48063aff5f4d
Jose Manuel Escobedo Lopez <teco@Joses-MacBook-Air.local>
2023-12-27 21:25:51 -0600

    expose set-cookie in front

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"strings"
 
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/tecolotedev/stori_back/config"
@@ -21,6 +22,15 @@ func main() {
 		AllowOrigins:     "http://localhost:3000, https://stori-front.tecolotedev.com",
 		AllowHeaders:     "Origin, Content-Type, Accept",
 		AllowCredentials: true,
+		AllowMethods: strings.Join([]string{
+			fiber.MethodGet,
+			fiber.MethodPost,
+			fiber.MethodHead,
+			fiber.MethodPut,
+			fiber.MethodDelete,
+			fiber.MethodPatch,
+		}, ","),
+		ExposeHeaders: "Set-Cookie",
 	}))
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{"ok": true, "message": "api is working"})
